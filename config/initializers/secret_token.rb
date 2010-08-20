ca5548d3dbb7b5948acd64a49e472d450434fab1
holtonma <holtonma@gmail.com>
2010-08-19 21:15:15 -0700

    OMG, Rails3 and Ruby 1.9.2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Golfapp::Application.config.secret_token = '2f259cbb465c1a7bc824274111dbc5a06748e760403a0a64e99a8493c5af6b1f62a4feaad750cbae40dd6c3721c2a6dee619cbfadcc1daefb7d5bde6208bfd4c'
